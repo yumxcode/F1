87ff105affcbd6ad221bc5e58bcfdf5de13fc447
robot <s8787@gitee.com>
2026-04-27 09:48:20 +0800

    增加测试代码

--- src/pkg/pkg1/pkg_main.cc
+++ src/pkg/pkg1/pkg_main.cc
@@ -5,6 +5,7 @@
 #include "joy_stick_module/joy_stick_module.h"
 #include "control_module/control_module.h"
 #include "sim_module/sim_module.h"
+#include "ankle_identifier_module/ankle_identifier_module.h"
 
 static std::tuple<std::string_view, std::function<aimrt::ModuleBase*()>>
     aimrt_module_register_array[]{
@@ -23,6 +24,10 @@ static std::tuple<std::string_view, std::function<aimrt::ModuleBase*()>>
         {"SimModule",
          []() -> aimrt::ModuleBase* {
            return new xyber_x1_infer::sim_module::SimModule();
+         }},
+        {"AnkleIdentifierModule",
+         []() -> aimrt::ModuleBase* {
+           return new xyber_x1_infer::ankle_identifier_module::AnkleIdentifierModule();
          }},
     };
 
